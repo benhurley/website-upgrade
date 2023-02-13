16574f051e878bf2b5d0390ea59675bdea1c0b3b
Benjamin Hurley <benfromtech@gmail.com>
2023-02-13 10:29:54 -0500

    update homepage copy

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -73,7 +73,7 @@ export const Home = () => {
         <AvatarContainer>
           <Image alt="headshot of ben" src={monkeHeadshot} />
         </AvatarContainer>
-        <Description>I create interfaces that closely match your design system.</Description>
+        <Description>I build interfaces that closely match your design system.</Description>
         <CTAContainer>
           <ShowMeButton onClick={() => navigate("/portfolio")}>Show Me</ShowMeButton>
           <ReachOutButton href="mailto:webdevbyben@gmail.com">Reach Out</ReachOutButton>
