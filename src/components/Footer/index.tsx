c7335f680e8071dd3d1be0f8f3328b11d8c07398
Benjamin Hurley <benfromtech@gmail.com>
2023-02-20 17:24:32 -0500

    update footer copy

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,7 +11,7 @@ font-size: 10px
 export const Footer = () => {
     return (
         <Container>
-            <p>2023, All Rights Reserved.</p>
+            <p>Created with ♥ by Ben, 2023.</p>
         </Container>
     )
 }
