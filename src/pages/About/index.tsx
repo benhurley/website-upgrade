f2c0cdbb7245bd5e8db622ddf891ec5e70871d13
Ben Hurley <benjaminhurley@pm.me>
2024-04-08 23:20:20 -0400

    feat: imporve about and contact pages

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -3,12 +3,17 @@ import { PrimaryLinkButton } from "../../components/Buttons/PrimaryLinkButton";
 import { FadeInComponent } from "../../helpers/FadeInComponent";
 import { SlideInText } from "../../helpers/SlideInText";
 import { SecondaryLinkButton } from "../../components/Buttons/SecondaryLinkButton";
+import ReactCardFlip from 'react-card-flip';
+import { useState } from "react";
+
+import artHeadshot from "../../img/me.webp";
+import artHeadshotReversed from "../../img/me-reversed.webp";
 
 const Container = styled.div`
 display: block;
 margin-left: auto;
 margin-right: auto;
-margin-top: 64px;
+margin-top: 32px;
 max-width: 525px;
 padding: 0px 20px;
 `
@@ -22,7 +27,8 @@ text-wrap: balance;
 `;
 
 const CTAContainer = styled.div`
-margin-top: 10px;
+margin-top: 16px;
+margin-bottom: 48px;
 display: inline-block;
 line-height: 78px;
 `
@@ -43,34 +49,62 @@ const Subtitle = styled.h2`
 margin-top: 64px;
 `
 
+const Image = styled.img`
+border-radius: 100%;
+height: 150px;
+width: 150px;
+cursor: pointer;
+border: 2px solid;
+border-color: white;
+`
+
+
 export const About = () => {
+    const [isFlipped, setIsFlipped] = useState(false);
+
     return (
         <FadeInComponent>
             <FadeInComponent timeout={250}>
                 <SlideInText text="About Me" size="h1" />
+                <ReactCardFlip
+                    infinite
+                    isFlipped={isFlipped}
+                    flipDirection="horizontal"
+                    flipSpeedFrontToBack={2}
+                    flipSpeedBackToFront={2}
+                >
+                    <Image
+                        alt="ben's ai-generated profile picture (front)"
+                        src={artHeadshot} onClick={() => setIsFlipped(!isFlipped)}
+                    />
+                    <Image
+                        alt="ben's ai-generated profile picture (back)"
+                        src={artHeadshotReversed} onClick={() => setIsFlipped(!isFlipped)}
+                    />
+                </ReactCardFlip>
                 <Container>
                     <Description>
-                       2017 - First job at an ad giant, learned about the cloud (AWS).
+                        Got my first job at an ad giant, learned about the cloud (AWS).
                     </Description>
                     <Description>
-                        2019 - Went into consulting to focus on building in AWS.
+                        Went into consulting to focus on building in AWS.
                     </Description>
                     <Description>
-                        2019 - Consulting made me go frontend (kicking and screaming).
+                        Consulting made me go frontend (kicking and screaming).
                     </Description>
                     <Description>
-                        2020 - Ended up really enjoying frontend, joined a DTC startup.
+                        Ended up really liking frontend actually.
                     </Description>
                     <Description>
-                        2023 - Rebuilt most of their web app over 3 years.
+                        Joined a startup & rebuilt their site over several years.
                     </Description>
                     <Description>
-                        2024 - Frontend specialist at large, tackling the gaming industry.
+                        Currently tackling the gaming industry.
                     </Description>
                 </Container>
             </FadeInComponent>
             <FadeInComponent>
-            <Subtitle>Want to see my work?</Subtitle>
+                <Subtitle>Want to see my work?</Subtitle>
                 <CTAContainer>
                     <WebsitesButton href="/websites">Websites</WebsitesButton>
                     <ChatbotsButton href="/chatbots">Chatbots</ChatbotsButton>
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -2,22 +2,54 @@ import styled from "styled-components";
 import { FadeInComponent } from "../../helpers/FadeInComponent";
 import { SlideInText } from "../../helpers/SlideInText";
 import { SecondaryLinkButton } from "../../components/Buttons/SecondaryLinkButton";
+import ReactCardFlip from 'react-card-flip';
+import { useState } from "react";
+
+import artHeadshot from "../../img/me.webp";
+import artHeadshotReversed from "../../img/me-reversed.webp";
 
 const LinkContainer = styled.div`
-margin: 70px 0px 70px 0px;
+margin: 50px 0px 70px 0px;
+`
+
+const Image = styled.img`
+border-radius: 100%;
+height: 150px;
+width: 150px;
+cursor: pointer;
+border: 2px solid;
+border-color: white;
 `
 
 export const Contact = () => {
+    const [isFlipped, setIsFlipped] = useState(false);
+
     return (
         <FadeInComponent>
             <FadeInComponent timeout={250}>
                 <SlideInText text="Contact" size="h1" />
             </FadeInComponent>
-                <FadeInComponent timeout={400}>
-                    <LinkContainer>
+            <ReactCardFlip
+                infinite
+                isFlipped={isFlipped}
+                flipDirection="horizontal"
+                flipSpeedFrontToBack={2}
+                flipSpeedBackToFront={2}
+            >
+                <Image
+                    alt="ben's ai-generated profile picture (front)"
+                    src={artHeadshot} onClick={() => setIsFlipped(!isFlipped)}
+                />
+                <Image
+                    alt="ben's ai-generated profile picture (back)"
+                    src={artHeadshotReversed} onClick={() => setIsFlipped(!isFlipped)}
+                />
+            </ReactCardFlip>
+            <FadeInComponent timeout={400}>
+                <LinkContainer>
                     <SecondaryLinkButton rel="noopener noreferrer" target="_blank" href={'mailto:justbenfyi@pm.me'}>send me an email</SecondaryLinkButton>
-                    </LinkContainer>
-                </FadeInComponent>
+                </LinkContainer>
+            </FadeInComponent>
         </FadeInComponent>
     );
 }
